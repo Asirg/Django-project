5d9287f67391a7a40e3ca6686d7b9fbff81ec743
Asirg <vojt.tieg295i@gmail.com>
2025-02-13 21:55:03 +0200

    materials about urls and templates

--- src/base/views.py
+++ src/base/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def HomeView(request):
+    return render(request, 'base/home.html')
+
+def RoomView(request, pk):
+    return render(request, 'base/room.html', context={'pk':pk})
