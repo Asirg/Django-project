d30172282036cdfdf518b33e04a7b9882fb7a97a
Asirg <vojt.tieg295i@gmail.com>
2025-02-18 12:21:22 +0200

    set base admin display for models

--- src/base/admin.py
+++ src/base/admin.py
@@ -4,16 +4,16 @@ from . import models
 
 @admin.register(models.Technology)
 class TechnologyAdmin(admin.ModelAdmin):
-    ...
+    list_display = ['name']
 
 @admin.register(models.Industry)
 class IndustryAdmin(admin.ModelAdmin):
-    ...
+    list_display = ['name']
 
 @admin.register(models.Domain)
 class DomainAdmin(admin.ModelAdmin):
-    ...
+    list_display = ['name']
 
 @admin.register(models.Specialization)
 class SpecializationAdmin(admin.ModelAdmin):
-    ...
+    list_display = ['name']
--- src/users/admin.py
+++ src/users/admin.py
@@ -4,8 +4,35 @@ from . import models
 
 @admin.register(models.UserSkill)
 class UserSkillAdmin(admin.ModelAdmin):
-    ...
+    list_display = [
+        'technology__name', 'score'
+    ]
 
 @admin.register(models.UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    ...
+    list_display = [
+        'id', 
+        'user__username', 
+        'city',
+    ]
+
+    fieldsets = [
+        (
+            None,
+            {"fields":['user']}
+        ),
+        (
+            "About User",
+            {"fields":[
+                ('city', 'country'),
+                'description'
+            ]}
+        ),
+        (
+            "Profession",
+            {"fields":[
+                ('domain', 'primary_specialization'),
+                ('secondary_specializations', 'skills')
+            ]}
+        )
+    ]
